66a9e4c07acdc257ed9d4fd9df60465abdba6263
aofleejay <aofleejay@hotmail.com>
2017-01-01 02:36:54 +0700

    add Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import Home from './Home'
+import Header from './Header'
 
 class App extends Component {
   render () {
     return (
-      <Home />
+      <div>
+        <Header />
+        <Home />
+      </div>
     )
   }
 }
